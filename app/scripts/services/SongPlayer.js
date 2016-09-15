d53db431da18e6b39e270f254890550c14923abc
Yuriy Ivanov <ymivanov@icloud.com>
2016-09-15 03:14:07 -0400

    Checkpoint 8 Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,11 +1,11 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
           var SongPlayer = {};
           /**
-          * @desc currentSong object
+          * @desc gets the object populated with the current album information
           * @type {Object}
           */
-          var currentSong = null;
+          var currentAlbum = Fixtures.getAlbum();
           /**
           * @desc Buzz object audio file
           * @type {Object}
@@ -20,16 +20,20 @@
           var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
                   
-            currentSong = song;
+            SongPlayer.currentSong = song;
           };
-         
+          /**
+          * @desc SongPlayer.currentSong object
+          * @type {Object}
+          */
+          SongPlayer.currentSong = null;
           /**
           * @function playSong
           * @desc plays the song 
@@ -38,26 +42,65 @@
           var playSong = function(song) {
               currentBuzzObject.play();
               song.playing = true;
-          }
+          };
+          /**
+          * @function getSongIndex
+          * @desc returns the index of the current song
+          * @param {Object} song
+          * @returns {Number}
+          */
+          var getSongIndex = function(song) {
+              return currentAlbum.songs.indexOf(song);
+          };
+          /**
+          
+          * @function play
+          * @desc Play current or new song
+          * @param {Object} song
+          */
           SongPlayer.play = function(song) {
-        
-              if (currentSong !== song) {
+              song = song || SongPlayer.currentSong;
+              if (SongPlayer.currentSong !== song) {
                   setSong(song);
                   playSong(song);
               }
-              else if(currentSong === song) {
+              else if(SongPlayer.currentSong === song) {
                   if(currentBuzzObject.isPaused()) {
                       playSong(song);
                   }
               }
           };
-         SongPlayer.pause = function(song) {
+          /**
+          * @function pause
+          * @desc Pause current song
+          * @param {Object} song
+          */
+          SongPlayer.pause = function(song) {
+             song = song || SongPlayer.currentSong;
              currentBuzzObject.pause();
              song.playing = false;
-         };
-
+          };
+          /**
+          * @function previous
+          * @description switches to previous song in album
+          */
+          SongPlayer.previous = function() {
+             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+              currentSongIndex--;
+              
+              if (currentSongIndex <0) {
+                  currentBuzzObject.stop();
+                  SongPlayer.currentSong.playing = null;
+              }
+              else {
+                  var song = currentAlbum.songs[currentSongIndex];
+                  setSong(song);
+                  playSong(song);
+              }
+          };
+ 
       return SongPlayer;
-      }
+   }
  
      angular
          .module('blocJams')
